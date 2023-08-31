adc0f1888f0451a23ac9ab57b144b11fd605278c
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-08-31 17:43:23 -0500

    updated updated the db folder with a schema.sql and seeds.sql file

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+-- check to make sure you want these to go to null --
+CREATE TABLE role (
+id INT AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT, 
+PRIMARY KEY(id),
+FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT, 
+PRIMARY KEY(id),
+FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+FOREIGN KEY (manager_id)
+    REFERENCES employee(id)  
+);
